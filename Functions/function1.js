17c8fbcbef0cfef09abe4d09e2b4d8a922b32744
haroonahmed786 <Haroon65ahmed@gmail.com>
2022-09-02 14:55:34 +0500

    javascript(functions)

---
+++ Functions/function1.js
@@ -0,0 +1,21 @@
+
+function functionName(){
+	document.write("Hello Haroon...! ");
+}
+functionName();
+document.write("<br>");
+functionName();
+document.write("<br>");
+functionName();
+document.write("<br>");
+
+
+function hello(){
+	document.write("Hi! siddiq   How Are You....?");
+}
+hello();
+document.write("<br>");
+hello();
+document.write("<br>");
+hello();
+
