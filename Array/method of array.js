0f833a91bfd59ca89b06f2587ac886ff722a5c9d
haroonahmed786 <Haroon65ahmed@gmail.com>
2022-09-03 10:36:26 +0500

    Array & Array of method

---
+++ Array/method of array.js
@@ -0,0 +1,34 @@
+
+               var Name = [ ' Siddique ' , ' Ahmed ' , ' Usman '  , ' Ali ' ]
+               
+               document.write(Name);
+                document.write('<br>');
+               
+                var Name1 = [ ' Siddique ' , ' Ahmed ' , ' Usman '  , ' Ali ' ]
+                document.write('<br>');
+                  Name1.pop();
+                  document.write(Name1);
+
+
+                  var Name2 = [ ' Siddique ' , ' Ahmed ' , ' Usman '  , ' Ali ' ]
+                  Name2.shift();
+                  document.write('<br>');
+                  document.write('<br>');
+                  document.write(Name2);
+
+
+                   var Name3 = [ ' Siddique ' , ' Ahmed ' , ' Usman '  , ' Ali ' ]
+                  Name3.push('Waqas');
+                  document.write('<br>');
+                  document.write('<br>');
+                  document.write(Name3);
+                   var Name4 = [ ' Siddique ' , ' Ahmed ' , ' Usman '  , ' Ali ' ]
+                  Name4.sort();
+                  document.write('<br>');
+                  document.write('<br>');
+                  document.write(Name4);
+                   var Name5 = [ ' Siddique ' , ' Ahmed ' , ' Usman '  , ' Ali ' ]
+                  Name5.reverse();
+                  document.write('<br>');
+                  document.write('<br>');
+                  document.write(Name5);
