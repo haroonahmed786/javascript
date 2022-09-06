d84fbd14cce362df46329f9d39be15c5aefc0817
haroonahmed786 <Haroon65ahmed@gmail.com>
2022-09-06 15:46:39 +0500

    Loop(while loop)

---
+++ loops/while loop.js
@@ -0,0 +1,10 @@
+var counterOne=10;
+while (counterOne > 0){
+	console.log(counterOne);
+	counterOne --;
+}
+var counterOne=0;
+while (counterOne < 10){
+	console.log(counterOne);
+	counterOne ++;
+}
