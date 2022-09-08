7520b8ed7964b426b1a10f53fe76c0584d99a90c
haroonahmed786 <Haroon65ahmed@gmail.com>
2022-09-08 12:36:25 +0500

    Javascript Scope (exercise)

---
+++ advance javascript/scope exercise.js
@@ -0,0 +1,48 @@
+function q1() {
+    var a = 5;
+    if(a > 1) {
+        a = 3;
+    }
+}
+
+q1();
+
+//#2
+var a = 0;
+function q2() {
+    a = 5;
+}
+
+function q22() {
+    alert(a);
+}
+q2();
+q22();
+
+//#3
+function q3() {
+    window.a = "hello";
+}
+
+
+function q32() {
+    alert(a);
+}
+q3();
+q32();
+
+//#4
+var a = 1;
+function q4() {
+    var a = "test";
+    alert(a);
+}
+q4();
+
+//#5
+var a = 2;
+if (true) {
+    var a = 5;
+    alert(a);
+}
+alert(a);
