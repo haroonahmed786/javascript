5d8de6fc608c58c9401b4fe7d79d9bef83c04229
haroonahmed786 <Haroon65ahmed@gmail.com>
2022-09-10 01:06:11 +0500

    adv javascript (root scope)

---
+++ advance javascript/scope/root scope.js
@@ -0,0 +1,23 @@
+var fun = 10;
+
+ function funFunction() {
+ 	var fun = "Helloo";
+ 	console.log(1,fun);
+ 	
+ }
+
+  function funerFunction() {
+ 	var fun = "Hiii!";
+ 	console.log(2,fun);
+ 	
+ }
+
+  function funestFunction() {
+ 	var fun = "Byeeeeeee";
+ 	console.log(3,fun);
+ 	
+ }
+ console.log(fun);
+ funFunction();
+ funerFunction();
+ funestFunction();
