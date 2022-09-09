63139457371768bbb9409844660d48b686f2e5dd
haroonahmed786 <Haroon65ahmed@gmail.com>
2022-09-10 01:05:09 +0500

    adv javascript(let &  const)

---
+++ advance javascript/let and constant/let & const.js
@@ -0,0 +1,9 @@
+const player = "boby";
+let experience = 100;
+var wizardlevel = false;
+
+ if(experience > 100){
+ 	var wizardlevel = true;
+ 	console.log('inside', wizardlevel);
+ }
+ 	console.log('outside', wizardlevel);
