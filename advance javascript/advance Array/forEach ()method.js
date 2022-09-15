9dc10df9442731285590f7a488c1ed62d6b5ddec
haroonahmed786 <Haroon65ahmed@gmail.com>
2022-09-15 15:14:54 +0500

    adv array forEach()method

---
+++ advance javascript/advance Array/forEach ()method.js
@@ -0,0 +1,20 @@
+var x = ["ahmed" , "ali" , "waqas"];
+
+x.forEach(function(value,index) {
+	document.write(index+"     "+value);
+});
+
+//
+ const array = [1,2,10,16];
+
+ const double = []
+
+ const newaArray =array.forEach((num)=>{
+
+ 	double.push(num*2);
+ });
+
+ console.log(double);
+
+
+ 
