98c95c1d7f226efc03ea6a6ff8681d5e698830a1
haroonahmed786 <Haroon65ahmed@gmail.com>
2022-09-15 15:13:44 +0500

    adv array map()method

---
+++ advance javascript/advance Array/map()method.js
@@ -0,0 +1,17 @@
+//map method//
+const numbers = [5,4,3,2];
+   
+ const mapArr = numbers.map(num => num*5);
+
+ console.log(mapArr);
+
+ //example map method//
+
+ const array = [5,10,15,20];
+   
+ const mapArray = array.map((num) =>{
+
+  num*2
+});
+
+ console.log(mapArray);
